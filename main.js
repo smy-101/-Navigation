8aee306fcf8483b1fb2a58238b16ec90e94556c8
smy-101 <shimy1011@foxmail.com>
2020-11-04 22:46:44 +0800

    初步建立前航模板

---
+++ main.js
@@ -0,0 +1,74 @@
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
+const x = localStorage.getItem('x')
+const xObject = JSON.parse(x)
+const hashMap = xObject || [
+    { logo: 'G', url: 'https://github.com' },
+    { logo: 'C', url: 'https://coding.net' }
+]
+
+const simplifyUrl = (url) => {
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www', '')
+        .replace(/\/.*/, '') // 删除 / 开头的内容
+}
+
+const render = () => {
+    $siteList.find('li:not(.last)').remove()
+    hashMap.forEach((node, index) => {
+        const $li = $(`<li>
+            <div class = "site">
+                <div class = "logo">${node.logo}</div>
+                <div class = "link">${simplifyUrl(node.url)}</div>
+                <div class = "close">
+                    <svg class="icon">
+                        <use xlink:href="#icon-close"></use>
+                    </svg>
+                </div>
+            </div>
+        </li>`).insertBefore($lastLi)
+        $li.on('click', () => {
+            window.open(node.url)
+        })
+        $li.on('click', '.close', (e) => {
+            console.log('关闭')
+            e.stopPropagation() // 阻止冒泡
+            hashMap.splice(index, 1)
+            render()
+        })
+    })
+}
+render()
+
+
+$('.addButton')
+    .on('click', () => {
+        let url = window.prompt('请问你要添加的网址是啥？')
+        if (url.indexOf('http') !== 0) {
+            url = 'https://' + url
+        }
+        console.log(url)
+        hashMap.push({
+            logo: simplifyUrl(url)[0].toUpperCase(),
+            url: url
+        })
+        console.log(hashMap)
+        render()
+    })
+
+window.onbeforeunload = () => {
+    console.log('22222')
+    const string = JSON.stringify(hashMap)
+    localStorage.setItem('x', string)
+}
+
+$(document).on('keypress', (e) => {
+    console.log(e.key)
+    const { key } = e  //key = e.key 的简写
+    for (let i = 0; i < hashMap.length; i++) {
+        if (hashMap[i].logo.toLowerCase() === key) {
+            window.open(hashMap[i].url)
+        }
+    }
+})
